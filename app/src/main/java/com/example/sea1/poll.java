d6a6e92b94f65cbf265a8b7759a237c6d1c0b008
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-11-05 11:04:21 +0530

    Fixed Barchat bug

--- app/src/main/java/com/example/sea1/poll.java
+++ app/src/main/java/com/example/sea1/poll.java
@@ -1,9 +1,15 @@
 package com.example.sea1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +22,9 @@ public class poll extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences = getSharedPreferences("water", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
         getWindow().setStatusBarColor(Color.WHITE);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
@@ -24,6 +33,26 @@ public class poll extends AppCompatActivity {
         setContentView(view);
 
         binding.submitButton.setOnClickListener(view1 -> {
+            int checkedId = binding.rgOptions.getCheckedRadioButtonId();
+
+            if (checkedId != -1) {
+                RadioButton selected = binding.rgOptions.findViewById(checkedId);
+                String value = selected.getText().toString();
+
+                int count = sharedPreferences.getInt(value, 0);
+                editor.putInt(value, count+1);
+                editor.apply();
+
+                Log.d("RADIOBUTTON", "Option selected: " + value);
+
+                Intent i = new Intent(poll.this, barchart.class);
+                startActivity(i);
+            } else {
+                Toast.makeText(poll.this, "Choose an option", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.graphButton.setOnClickListener(view1 -> {
             Intent i = new Intent(poll.this, barchart.class);
             startActivity(i);
         });
